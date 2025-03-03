912bf0bd5875535227726212350a2bcb1c99d410
Harshitha-tn <harshithatn10@gmail.com>
2025-03-03 16:27:27 +0530

    Update quoteweb.py

--- quoteweb.py
+++ quoteweb.py
@@ -3,45 +3,37 @@ from bs4 import BeautifulSoup
 import csv
 import pandas as pd
 
-# URL
+
 URL = "http://www.values.com/inspirational-quotes"
 r = requests.get(URL)
-
-# Parse the HTML
 soup = BeautifulSoup(r.content, 'html5lib')
-
-# Check if we are able to find the section with quotes
 table = soup.find('div', attrs={'id': 'all_quotes'})
 if table is None:
     print("Couldn't find the quotes section. Check if the HTML structure has changed.")
 else:
     print("Quotes section found.")
 
-# List to store quotes
 quotes = []
-
-# Loop through and extract quotes
 for row in table.findAll('div', attrs={'class': 'col-6 col-lg-3 text-center margin-30px-bottom sm-margin-30px-top'}):
     quote = {}
-    theme = row.find('h5')  # Find the theme (quote category)
+    theme = row.find('h5')  
     if theme:
         quote['theme'] = theme.text
     else:
         print("Theme not found for a quote.")
     
-    url = row.find('a')  # Find the link
+    url = row.find('a') 
     if url and 'href' in url.attrs:
         quote['url'] = url['href']
     else:
         print("URL not found for a quote.")
     
-    img = row.find('img')  # Find the image
+    img = row.find('img')  
     if img and 'src' in img.attrs:
         quote['img'] = img['src']
     else:
         print("Image not found for a quote.")
     
-    # Extract quote lines and author from the alt text
     if img and 'alt' in img.attrs:
         alt_text = img['alt']
         if " #" in alt_text:
